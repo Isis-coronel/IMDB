037c805d7e80d83f06c6bd59f30a588d55ab3b96
Isis <isisd.coronel@gmail.com>
2020-07-12 21:36:31 +0200

    Created JSON

--- Imdb/main.js
+++ Imdb/main.js
@@ -2,7 +2,15 @@
 exports.__esModule = true;
 var imdb_1 = require("./imdb");
 var movie_1 = require("../Movie/movie");
+var fs = require("fs");
 var peli1 = new movie_1.Movie("Joker", 2019, "american", "drama");
 var peli2 = new movie_1.Movie("Batman", 2015, "american", "drama");
 var imdbs = new imdb_1.Imdb([peli1, peli2]);
 console.log(imdbs.movies);
+var dataJson = JSON.stringify(imdbs);
+fs.writeFile("imdbBBDD.json", dataJson, function (err) {
+    if (err) {
+        console.log(err);
+    }
+});
+console.log(dataJson);
--- Imdb/main.ts
+++ Imdb/main.ts
@@ -1,8 +1,19 @@
 import {Imdb} from "./imdb"
 import {Movie} from "../Movie/movie"
 
+import * as fs from 'fs';
 let peli1 = new Movie ("Joker", 2019, "american", "drama");
 let peli2 = new Movie ("Batman", 2015, "american", "drama");
 
 let imdbs = new Imdb([peli1,peli2]);
 console.log(imdbs.movies)
+
+let dataJson = JSON.stringify(imdbs);
+
+fs.writeFile("imdbBBDD.json", dataJson, (err) => {
+    if (err) {
+        console.log(err);
+    }
+})
+
+console.log(dataJson);
